14f69507e31735de29d2b26f36a005d5076f937c
Felix Rodriguez <rodriguezfelix17@gmail.com>
2021-12-04 11:35:41 -0400

    changes

--- src/router/product.js
+++ src/router/product.js
@@ -2,23 +2,17 @@ import express from "express";
 import {
   getProducts,
   findProductById,
+  createProduct,
+  updateProduct,
+  deleteProductById,
 } from "../controller/productController.js";
 
 const productRouter = express.Router();
 
 productRouter.get("/product", getProducts);
 productRouter.get("/product/:id", findProductById);
-
-productRouter.post("/product", function (req, res) {
-  res.send("save products");
-});
-
-productRouter.put("/product", function (req, res) {
-  res.send("update products");
-});
-
-productRouter.delete("/product", function (req, res) {
-  res.send("delete products");
-});
+productRouter.post("/product", createProduct);
+productRouter.put("/product", updateProduct);
+productRouter.delete("/product", deleteProductById);
 
 export { productRouter };
